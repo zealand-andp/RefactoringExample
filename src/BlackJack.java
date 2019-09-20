f153097ff8e41a2583935f283fccbf3589a31d71
Anders Petersen <AndP@easj.dk>
2019-09-20 11:54:15 +0200

    Method extracted to remove duplication

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -1,21 +1,21 @@
 public class BlackJack {
     public boolean dealerWins(Hand playerHand)
     {
-        int playerHandValue =0;
-        for (Card card : playerHand)
-        {
-            playerHandValue += determineCardValue(card.Value, playerHand);
-        }
+        int playerHandValue = getHandValue(playerHand);
 
         Hand dealerHand = DealerManager.Hand;
-        int dealerHandValue =0;
-        for (Card card : dealerHand)
-        {
-            dealerHandValue += determineCardValue(card.Value, dealerHand);
-        }
+        int dealerHandValue = getHandValue(dealerHand);
 
         return dealerHandValue>=playerHandValue;
 
+    }
+
+    private int getHandValue(Hand playerHand) {
+        int playerHandValue = 0;
+        for (Card card : playerHand) {
+            playerHandValue += determineCardValue(card.Value, playerHand);
+        }
+        return playerHandValue;
     }
 
     public int determineCardValue(int value, Hand hand) {
